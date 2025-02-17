1840fc578f04fb14a1c62ec1e64ef674766cb0d5
devCaiodias <caiodiasmartins12@gmail.com>
2025-02-17 20:43:26 -0300

    adicionei  o buttom para ir na pagina do clima da cidade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
+'use client'
 import {archivo} from '../../public/font/font'
 
 export default function Home() {
   return (
     <>
       {/* main title */}
-      <h1 className={`font-sans text-8xl text-white w-8/12 ${archivo.className} ml-16 pb-7`}>See the weather in your city</h1>
+      <h1 className={`text-8xl text-white w-8/12 ${archivo.className} ml-16 pb-7`}>See the weather in your city</h1>
 
       {/* text input */}
-      <input type="text" placeholder="enter your city" className='ml-28 bg-transparent border-black border-2 py-2 rounded-full text-center text-2xl text-black hover:border-4' />
+      <form className='flex items-center'>
+        <input
+        type="text"
+        placeholder="enter your city"
+        className='ml-20 bg-transparent border-black border-2 py-2 rounded-full text-center text-2xl text-black hover:border-4' />
+        <button type='submit' className='m-5 border-2 p-3 rounded-full border-black hover:border-white'>see weather</button>
+      </form>
     </>
   );
 }
