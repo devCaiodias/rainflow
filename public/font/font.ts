de86ccb2491dfbaac9174a1d1ec572aca1d5acb8
devCaiodias <caiodiasmartins12@gmail.com>
2025-02-17 10:04:16 -0300

    terminei  a pagina do home completa e add fonte

---
+++ public/font/font.ts
@@ -0,0 +1,7 @@
+import { Archivo_Black } from 'next/font/google';
+
+export const archivo = Archivo_Black({
+    subsets: ['latin'],
+    // Peso da Font
+    weight: ['400']
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import {archivo} from '../../public/font/font'
 
 export default function Home() {
   return (
     <>
-      
+      {/* main title */}
+      <h1 className={`font-sans text-8xl text-white w-8/12 ${archivo.className} ml-16 pb-7`}>See the weather in your city</h1>
+
+      {/* text input */}
+      <input type="text" placeholder="enter your city" className='ml-28 bg-transparent border-black border-2 py-2 rounded-full text-center text-2xl text-black hover:border-4' />
     </>
   );
 }
